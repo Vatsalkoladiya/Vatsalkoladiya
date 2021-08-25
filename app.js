fdf33648a63ec425d5f90699fe3085d1569b60a5
Vatsalkoladiya <80945341+Vatsalkoladiya@users.noreply.github.com>
2021-08-25 12:05:58 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Joker from "./Joker";
+import "antd/dist/antd.css";
+
+const App = () => {
+  return (
+    <>
+      <Joker />
+    </>
+  );
+};
+
+export default App;
